d2898bce02d474175e39c4c222431285c6aa88d6
Ilbert130 <ilbertca27@gmail.com>
2023-08-03 09:35:37 -0400

    post controller created

---
+++ src/main/java/com/system/blog/controllers/PostController.java
@@ -0,0 +1,28 @@
+package com.system.blog.controllers;
+
+import com.system.blog.dtos.PostDTO;
+import com.system.blog.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PostDTO> savePost(@RequestBody PostDTO postDTO){
+        return new ResponseEntity<>(postService.createPost(postDTO), HttpStatus.CREATED);
+    }
+}
